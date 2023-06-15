52403b9471b9f1d77a728322d82a20553c3d6002
souravsubba92 <104068522+souravsubba92@users.noreply.github.com>
2023-06-15 14:41:19 +0530

    turned into swtich state

--- index.js
+++ index.js
@@ -2,28 +2,44 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 
 const INCREMENT = "INCREMENT";
+
 const INCREMENTBYAMOUNT = "INCREMENTBYAMOUNT";
+
+const INIT = "INIT";
+
 const store = createStore(reducer, applyMiddleware(logger.default));
 
 const history = [];
 
 //reducer
 function reducer(state = { amount: 1 }, action) {
-  if (action.type === INCREMENT) {
-    //immutability
-    return {
-      ...state,
-      amount: state.amount + 1,
-    };
-  }
+  // if (action.type === INCREMENT) {
+  //   //immutability
+  //   return {
+  //     ...state,
+  //     amount: state.amount + 1,
+  //   };
+  // }
 
-  if (action.type === INCREMENTBYAMOUNT) {
-    return {
-      ...state,
-      amount: state.amount + action.payload,
-    };
+  // if (action.type === INCREMENTBYAMOUNT) {
+  //   return {
+  //     ...state,
+  //     amount: state.amount + action.payload,
+  //   };
+  // }
+
+  switch (action.type) {
+    case INIT:
+      return { amount: action.payload };
+    case INCREMENT:
+      return { amount: state.amount + 1 };
+
+    case INCREMENTBYAMOUNT:
+      return { amount: state.amount + action.payload };
+
+    default:
+      return state;
   }
-  return state;
 }
 
 //global state
@@ -47,9 +63,14 @@ function incrementByAmount(value) {
   return { type: INCREMENTBYAMOUNT, payload: value };
 }
 
+function initUser(value) {
+  return { type: INIT, payload: value };
+}
+
 setInterval(() => {
   //   store.dispatch({ type: "incrementByAmount", payload: 10 });
   //   store.dispatch(increment());
-  store.dispatch(incrementByAmount(10));
-}, 7000);
+  // store.dispatch(incrementByAmount(10));
+  store.dispatch(initUser(500));
+}, 5000);
 // console.log(store.getState());
